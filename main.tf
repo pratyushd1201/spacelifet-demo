43579f97b197fe016bfce31b18b72e0d2430c8c3
pratyushd1201 <pratyush.d1201@gmail.com>
2026-02-12 18:58:23 +0530

    removed key

--- main.tf
+++ main.tf
@@ -6,8 +6,6 @@ resource "aws_instance" "my_ec2" {
   ami           = "ami-0f58b397bc5c1f2e8"  # Amazon Linux 2 AMI (ap-south-1)
   instance_type = "t2.micro"
 
-  key_name = "ec2_key"   # Replace with your key pair name
-
   tags = {
     Name = "MyTerraformEC2"
   }
